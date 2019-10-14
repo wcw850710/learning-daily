b64e4f710f5b9913f6ea0c7c716145c9221bd367
Frank <tgfc064@tgfc.tw>
2019-10-14 10:14:28 +0800

    firestore init

--- public/bg.js
+++ public/bg.js
@@ -1,15 +1,11 @@
 const firebaseConfig = {
     apiKey: 'AIzaSyCtx5J2x18iLlMIDURgMecmaVkfh6_OSeQ',
     authDomain: 'pagemaker-23fec.firebaseapp.com',
-    databaseURL: 'https://pagemaker-23fec.firebaseio.com',
     projectId: 'pagemaker-23fec',
-    storageBucket: 'pagemaker-23fec.appspot.com',
-    messagingSenderId: '693822124792',
-    appId: '1:693822124792:web:c5bfb6f13bdd1e9d56410d',
 }
 
 const app = firebase.initializeApp(firebaseConfig)
-const db = app.database()
+const db = app.firestore()
 
 function $db() {
     return db
