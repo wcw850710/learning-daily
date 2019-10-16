30c03c9318744571a545f292624c54a99b723a49
俊緯 <aaza0710@gmail.com>
2019-10-16 22:38:43 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const path = require('path')
+const UglifyJS = require('uglify-es')
+const htmlMinify = require('html-minifier').minify
 
 // Generate pages object
 const pagesObj = {}
 
-const chromeName = ['popup', 'options']
+const chromeName = ['popup' /*, 'options'*/]
 
 chromeName.forEach(name => {
     pagesObj[name] = {
@@ -18,6 +20,31 @@ const plugins = [
     {
         from: path.resolve('src/manifest.json'),
         to: `${path.resolve('dist')}/manifest.json`,
+    },
+    {
+        from: path.resolve('src/bg/'),
+        to: `${path.resolve('dist')}/[name].[ext]`,
+        transform(content) {
+            return UglifyJS.minify(content.toString()).code
+        },
+        ignore: ['*.html'],
+    },
+    {
+        from: path.resolve('src/bg/bg.html'),
+        to: `${path.resolve('dist')}/bg.html`,
+        transform(content) {
+            return htmlMinify(content.toString(), {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+                minifyCSS: true,
+                minifyJS: true,
+                removeComments: true,
+            })
+        },
+    },
+    {
+        from: path.resolve('src/icons/'),
+        to: `${path.resolve('dist')}/[name].[ext]`,
     },
 ]
 
